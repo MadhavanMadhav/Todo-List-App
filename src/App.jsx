4f9ad3b82da3bde7c3dfa29425fffb9475ee3163
MadhavanMadhav <madhavan150901@gmail.com>
2025-03-19 12:55:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='Add'>
+      <h1>My Todos</h1>
+      <div></div>
+    </div>
+  )
+}
+
+export default App
